73b4eae98f7b6f7199fbf3981cf8b1008d359cc3
Muhammad Imran Bin Ridzuan <imran.ridzuan@shopee.com>
2022-07-12 17:34:34 +0800

    Add logic to determine winner

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ const CHOICES = [ROCK, PAPER, SCISSORS]
 function computerPlay() {
     return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === ROCK && computerSelection === SCISSORS) 
+    || (playerSelection === SCISSORS && computerSelection === PAPER)
+    || (playerSelection === PAPER && computerSelection === ROCK)) {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else if ((computerSelection === ROCK && playerSelection === SCISSORS) 
+    || (computerSelection === SCISSORS && playerSelection === PAPER)
+    || (computerSelection === PAPER && playerSelection === ROCK)) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    } else {
+        return "It's a draw!"
+    }
+}
